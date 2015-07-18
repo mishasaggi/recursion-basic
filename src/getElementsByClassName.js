b62d60eac06c083525791f014c16bde3418dfd1e
mishasaggi <monisha.saggi@gmail.com>
2015-07-18 02:33:36 -0500

    Updated function to use element.childNodes instead of children as specified in requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,10 +5,12 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-	//use element.children, element.classList, element.className, node.contains()
+	//use element.childNodes instead of element.children, element.classList, 
+	//node.contains() 
+	//Could not use document.body
 	var result = [];
   	function findClass(level) {
-  		var children_array = level.children;
+  		var children_array = level.childNodes;
 
   		//terminal case for no nodes in element being searched
   		if(children_array.length == 0) {
@@ -18,11 +20,11 @@ var getElementsByClassName = function(className){
   		//base case... I guess... because if there are no more levels to go down, 
   		//the section after won't run and it will return
   		for (var i = 0; i < children_array.length; i++) {
-  			if (children_array[i].classList.contains(className)) {
+  			if (children_array[i].classList && children_array[i].classList.contains(className)) {
   			result.push(children_array[i]);
   		}
   		//using recursion, explore all levels until the the last child node on each branch
-  			if (children_array[i].children) {
+  			if (children_array[i].childNodes) {
   			findClass(children_array[i]);
   			}
   		}
